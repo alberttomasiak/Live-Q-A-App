3a00fdc874b0e05b7922dcae1fa90ae8da5254c9
iljaschoepen <ilja.schoepen@student.thomasmore.be>
2016-04-26 20:20:31 +0200

    Start add discussion

--- controllers/discussion.js
+++ controllers/discussion.js
@@ -1,16 +1,31 @@
-var Discussion = require('./../models/discussion');
+var Discussion = require('../models/discussion');
 
 // create a new discussion (req.body)
 function create(req, res, next) {
-    console.log('ilja');
+    console.log('Hi there');
     console.log(req.body);
-    /*var title = req.body.title;
+    
+    var title = req.body.title;
     var description = req.body.description;
-    var newDiscussion = new Discussion(title, description);
-    newDiscussion.save(); 
-  /*newDiscussion.save(function onDiscussionSaved(err, message) {
-    // we return the json version with cleaned up model to the user
-    res.send(discussion.toJSON());
-  });*/
+    
+    var newDiscussion = new Discussion ({
+        title: title,
+        description: description
+    });
+    
+    newDiscussion.save();
 }
 module.exports.create = create;
+
+// get one message by id (req.params.id)
+function getOne(req, res, next) {
+  Discussion.findOne({_id: req.params.id}, function onDiscussionFound(err, discussion) {
+    if(!discussion) {
+      res.status(404).send("Discussion not found");
+    }
+    console.log(discussion);
+    // we return the json version with cleaned up model to the user
+    res.send(discussion.toJSON());
+  });
+}
+module.exports.readOne = getOne;
--- routers/discussion.js
+++ routers/discussion.js
@@ -1,9 +1,12 @@
-var controller = require('./../controllers/discussion');
+var controller = require('../controllers/discussion');
 var express = require('express');
 var router = express.Router();
 
 //router.get('/', controller.read);
 //router.get('/:id', controller.readOne);
+router.get('/:id', function(req, res){
+        res.render('discussion');
+});
 //router.get('/room/:room', controller.readFromRoom);
 router.post('/create', controller.create);
 //router.put('/:id', controller.update);
@@ -14,3 +17,24 @@ router.get('/create', function(req, res){ // moet in controlller
 });
 
 module.exports = router;
+
+
+
+/*
+// example
+app.get('/product/:id', function(req, res){
+        Product.find({_id: req.params.id}, function (error, data) {
+                if(error){
+                        console.log(error);
+                } else {
+                        console.log("DATA :" + data[0]); //correct json object
+                        res.render('product',{
+                                title: 'Product Template',
+                                result: data[0]
+                                }
+                        );
+                }
+        });
+
+})
+*/
