9d207965c8ed13b07b84bd4f6643024048756a10
Albert Tomasiak <tomasiakalbert@gmail.com>
2016-05-03 12:32:06 +0200

    Passport register & login

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,58 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(app, passport) {
 
-// the root url, if requested we will render the index page
-router.get('/', function(req, res) {
-  res.render('index', {
-    title: 'Homepage',
-    //urls: ["users", "messages"]
-  });
-});
+	//landing 
+    app.get('/', function(req, res) {
+        res.render('pass.jade'); // load the index.ejs file
+    });
 
-module.exports = router;
+	app.get('/home', isLoggedIn, function(req, res){
+		res.render('index.jade');
+	});
+	
+	//login
+    app.get('/login', function(req, res) {
+        res.render('login.jade', { message: req.flash('loginMessage') }); 
+    });
+
+    //signup
+    app.get('/signup', function(req, res) {
+        res.render('signup.jade', { message: req.flash('signupMessage') });
+    });
+	
+	//process signup
+	app.post('/signup', passport.authenticate('local-signup',{
+		successRedirect : '/home', // redirect to secure profile section
+		failureRedirect : '/signup', // redirect to signup page on failure
+		failureFlash : true // allow flash messages
+	}));
+	
+	//process login
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/home',
+		failureRedirect : '/login',
+		failureFlash : true
+	}));
+
+    //profile
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.jade', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    //logout
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+	
+};
+
+// middleware to check if user is logged in
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
